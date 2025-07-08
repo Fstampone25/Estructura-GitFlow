7d3a389ad9db5ba69417b1f221405b27566506e2
Fstampone25 <facustampone27@gmail.com>
2025-07-07 23:33:52 -0300

    v0.1

---
+++ Calculadora_v0.1 develop.py
@@ -0,0 +1,10 @@
+
+numero1 = int(input("ingrese el primer numero: "))
+
+numero2 = int(input("ingrese el segundo numero: "))
+
+operacion = input("elegi la operacion + o -: ")
+
+if operacion == "+":
+    resultado = numero1 + numero2
+    print("el resultado es: ", resultado)
